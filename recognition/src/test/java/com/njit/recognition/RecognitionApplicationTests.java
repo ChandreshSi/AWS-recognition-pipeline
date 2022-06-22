37a8950adc87f51b9e97b0f6d5720c0fa3a4a947
Chandresh Sikarwar <cs62@njit.edu>
2022-06-22 18:34:23 -0400

    Change folder

---
+++ recognition/src/test/java/com/njit/recognition/RecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njit.recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
